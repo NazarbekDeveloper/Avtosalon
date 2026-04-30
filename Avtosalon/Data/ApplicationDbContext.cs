af8a7d994f37b4da0ecc03e9e03d402d5c10caa1
NazarbekDeveloper <bekzalatoy05@gmail.com>
2026-04-30 16:35:05 +0500

    Added Data

---
+++ Avtosalon/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Avtosalon.Models.Cars;
+using Avtosalon.Models.Katalog;
+using Avtosalon.Models.Persons;
+using Avtosalon.Models.Sales;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avtosalon.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        DbSet<Car> Cars { get; set; }
+        DbSet<Brand> Brands { get; set; }
+        DbSet<CarModel> CarModels { get; set; }
+        DbSet<Customer> Customers { get; set; }
+        DbSet<Employee> Employees { get; set; }
+        DbSet<Sale> Sales { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
--- Avtosalon/Program.cs
+++ Avtosalon/Program.cs
@@ -1,7 +1,12 @@
+using Avtosalon.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
